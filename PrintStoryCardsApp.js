ab435a0ab71e67f88df23dfba99a3095455824af
e047302 <e047302@mastercard.com>
2019-10-03 16:38:27 -0300

    added preliminaryestimate

--- PrintStoryCardsApp.js
+++ PrintStoryCardsApp.js
@@ -14,6 +14,7 @@
             '<div class="name">{Name}</div>' +
             '<div class="description">{Description}</div>' +
             '</div>' +
+            '<span class="preliminaryestimate">{[this.getPreliminaryEstimate(values)]}</span>' +
             '<span class="planestimate">{[this.getEstimate(values)]}</span>' +
             '</div></tpl>', {
                 getOwnerImage: function(values) {
@@ -24,11 +25,13 @@
                 },
                 getParentID: function(values) {
                     return values.WorkProduct && (':' + values.WorkProduct.FormattedID) || '';
+                },                
+                getPreliminaryEstimate: function(values) {                    
+                    return values.PreliminaryEstimate && values.PreliminaryEstimate.Name || 'None';
                 },
                 // Tasks have Estimate(s), Stories have PlanEstimate(s)
                 getEstimate: function(values) {
-                    // return values.Estimate || values.LeafStoryPlanEstimateTotal || 'None';
-                    return values.Estimate || values.PlanEstimate || 'None';
+                    return values.Estimate || values.LeafStoryPlanEstimateTotal || 'None';                    
                 }
             }
         )
@@ -69,10 +72,8 @@
             Ext.create('Rally.data.wsapi.Store', {
                 context: this.getContext().getDataContext(),
                 autoLoad: true,
-                // model: Ext.identityFn('PortfolioItem/Feature'),
-                model: Ext.identityFn('UserStory'),
-                // fetch: ['FormattedID', 'Name', 'Owner', 'Description', 'LeafStoryPlanEstimateTotal'],
-                fetch: ['FormattedID', 'Name', 'Owner', 'Description', 'PlanEstimate'],
+                model: Ext.identityFn('PortfolioItem/Feature'),
+                fetch: ['FormattedID', 'Name', 'Owner', 'Description', 'LeafStoryPlanEstimateTotal', 'PreliminaryEstimate'],
                 limit: (scope.getRecord()) ? 200 : 50,
                 listeners: {
                     load: this._onStoriesLoaded,
@@ -118,7 +119,7 @@
                     options = "toolbar=1,menubar=1,scrollbars=yes,scrolling=yes,resizable=yes,width=1000,height=500";
 
                     var css = '<style type="text/css"> '+ 
-        '.printcards{margin:5px;width:100%;height:100%}.printcards html{background-color:#fff;color:#000;font:14pt / 1.26;margin:0;padding:0}.printcards .header{margin:5px}.printcards .description{float:left;font:12pt NotoSans, Helvetica, Arial;margin:0.25em auto 0 auto;padding-left:1.0em;padding-right:1.0em;overflow-y:hidden;width:100%;word-wrap:break-word}.printcards .card-header{border:1px;border-bottom-style:solid;display:table-cell;height:40px;vertical-align:middle;width:4.3in}.printcards .name{font:28px ProximaNovaBold, Helvetica, Arial;padding-top:0.5em;text-align:center}.printcards .owner{float:right;height:40px}.printcards .ownerText{float:right;font:14pt / 1.26 NotoSans,Helvetica,Arial;margin-right:0.3em;margin-top:0.3em}.printcards .formattedid{float:left;font:14pt / 1.26 NotoSans,Helvetica,Arial;margin-left:0.25em;margin-top:0.3em}.printcards .planestimate{bottom:0.5em;position:absolute;right:0.5em}.printcards .content{height:2.4in;overflow:hidden;width:4.3in;color:black;padding-left:4px;padding-right:8px}.printcards body{background-color:#fff;margin:0;padding:0}.printcards .cb{clear:both}.printcards .artifact{background-color:#fff;border:2px solid #000;float:left;height:3.2in;margin:0.1in 0.1in 0.1in 0.1in;position:relative;overflow:hidden;width:4.3in}.print-page .printcards{width:920px !important;height:100% !important;overflow:hidden !important}.print-page .printcards .header{display:none}@media print{.printcards .pb{page-break-after:always;clear:both}} ' +
+        '.printcards{margin:5px;width:100%;height:100%}.printcards html{background-color:#fff;color:#000;font:14pt / 1.26;margin:0;padding:0}.printcards .header{margin:5px}.printcards .description{float:left;font:12pt NotoSans, Helvetica, Arial;margin:0.25em auto 0 auto;padding-left:1.0em;padding-right:1.0em;overflow-y:hidden;width:100%;word-wrap:break-word}.printcards .card-header{border:1px;border-bottom-style:solid;display:table-cell;height:40px;vertical-align:middle;width:4.3in}.printcards .name{font:28px ProximaNovaBold, Helvetica, Arial;padding-top:0.5em;text-align:center}.printcards .owner{float:right;height:40px}.printcards .ownerText{float:right;font:14pt / 1.26 NotoSans,Helvetica,Arial;margin-right:0.3em;margin-top:0.3em}.printcards .formattedid{float:left;font:14pt / 1.26 NotoSans,Helvetica,Arial;margin-left:0.25em;margin-top:0.3em}.printcards .planestimate{bottom:0.5em;position:absolute;right:0.5em}.printcards .preliminaryestimate{margin:0.25em auto 0 auto;padding-left:1.0em;bottom:0.5em;position:absolute}.printcards .content{height:2.4in;overflow:hidden;width:4.3in;color:black;padding-left:4px;padding-right:8px}.printcards body{background-color:#fff;margin:0;padding:0}.printcards .cb{clear:both}.printcards .artifact{background-color:#fff;border:2px solid #000;float:left;height:3.2in;margin:0.1in 0.1in 0.1in 0.1in;position:relative;overflow:hidden;width:4.3in}.print-page .printcards{width:920px !important;height:100% !important;overflow:hidden !important}.print-page .printcards .header{display:none}@media print{.printcards .pb{page-break-after:always;clear:both}} ' +
     '</style>';//document.getElementsByTagName('style')[0].innerHTML;
                     var title = "Print Features";
                     var printWindow = window.open('', '', options);
